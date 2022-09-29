ff21de606e85d830d20a8a7d5b0d6e6afadade98
Heiko Schaefer <heiko@rangun.de>
2022-09-29 08:17:57 +0200

    Initial commit

---
+++ src/main/java/de/rangun/reanimator/ReAnimatorMod.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2022 by Heiko Schäfer <heiko@rangun.de>
+ *
+ * This file is part of ReAnimator.
+ *
+ * ReAnimator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * ReAnimator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ReAnimator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.rangun.reanimator;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.VertexFormat;
+import net.minecraft.client.render.VertexFormats;
+import net.minecraft.util.math.Vec3d;
+
+@Environment(EnvType.CLIENT)
+public final class ReAnimatorMod implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+
+		WorldRenderEvents.BEFORE_DEBUG_RENDER.register((ctx) -> {
+
+			final Vec3d pos1 = new Vec3d(6d, -1d, -8d);
+			final Vec3d pos2 = new Vec3d(9d, 4d, -11d);
+			
+			final Vec3d pos3 = new Vec3d(1d, 1d, -8d);
+			final Vec3d pos4 = new Vec3d(0d, 0d, -7d);
+
+			RenderSystem.lineWidth(1.0f);
+			RenderSystem.depthMask(false);
+			RenderSystem.disableTexture();
+			RenderSystem.disableBlend();
+
+			RenderSystem.setShader(GameRenderer::getPositionColorShader);
+
+			renderCube(ctx, pos1, pos2, 255, 0, 0, 255);
+			renderCube(ctx, pos3, pos4, 0, 0, 255, 255);
+
+			RenderSystem.depthMask(true);
+			RenderSystem.enableBlend();
+			RenderSystem.enableTexture();
+		});
+	}
+
+	private void renderCube(final WorldRenderContext ctx, final Vec3d pos1, final Vec3d pos2, int red, int green,
+			int blue, int alpha) {
+
+		final Vec3d npos1 = pos1.getZ() < pos2.getZ() ? pos2 : pos1;
+		final Vec3d npos2 = pos1.getZ() < pos2.getZ() ? pos1 : pos2;
+
+		final Vec3d camera = ctx.camera().getPos();
+
+		final Tessellator tessellator = Tessellator.getInstance();
+		final BufferBuilder buffer = tessellator.getBuffer();
+
+		buffer.begin(VertexFormat.DrawMode.DEBUG_LINES, VertexFormats.POSITION_COLOR);
+
+		final Vec3d[] vertices = new Vec3d[] { new Vec3d(npos1.getX(), npos1.getY(), npos1.getZ() + 1d),
+				new Vec3d(npos1.getX(), npos1.getY(), npos2.getZ()),
+				new Vec3d(npos1.getX(), npos1.getY(), npos2.getZ()),
+				new Vec3d(npos2.getX() + 1d, npos1.getY(), npos2.getZ()),
+				new Vec3d(npos2.getX() + 1d, npos1.getY(), npos2.getZ()),
+				new Vec3d(npos2.getX() + 1d, npos1.getY(), npos1.getZ() + 1d),
+				new Vec3d(npos2.getX() + 1d, npos1.getY(), npos1.getZ() + 1d),
+				new Vec3d(npos1.getX(), npos1.getY(), npos1.getZ() + 1d),
+				new Vec3d(npos1.getX(), npos2.getY() + 1d, npos1.getZ() + 1d),
+				new Vec3d(npos1.getX(), npos2.getY() + 1d, npos2.getZ()),
+				new Vec3d(npos1.getX(), npos2.getY() + 1d, npos2.getZ()),
+				new Vec3d(npos2.getX() + 1d, npos2.getY() + 1d, npos2.getZ()),
+				new Vec3d(npos2.getX() + 1d, npos2.getY() + 1d, npos2.getZ()),
+				new Vec3d(npos2.getX() + 1d, npos2.getY() + 1d, npos1.getZ() + 1d),
+				new Vec3d(npos2.getX() + 1d, npos2.getY() + 1d, npos1.getZ() + 1d),
+				new Vec3d(npos1.getX(), npos2.getY() + 1d, npos1.getZ() + 1d),
+				new Vec3d(npos1.getX(), npos1.getY(), npos1.getZ() + 1d),
+				new Vec3d(npos1.getX(), npos2.getY() + 1d, npos1.getZ() + 1d),
+				new Vec3d(npos2.getX() + 1d, npos1.getY(), npos1.getZ() + 1d),
+				new Vec3d(npos2.getX() + 1d, npos2.getY() + 1d, npos1.getZ() + 1d),
+				new Vec3d(npos1.getX(), npos1.getY(), npos2.getZ()),
+				new Vec3d(npos1.getX(), npos2.getY() + 1d, npos2.getZ()),
+				new Vec3d(npos2.getX() + 1d, npos1.getY(), npos2.getZ()),
+				new Vec3d(npos2.getX() + 1d, npos2.getY() + 1d, npos2.getZ()) };
+
+		for (final Vec3d v : vertices) {
+
+			final Vec3d cv = v.subtract(camera);
+
+			buffer.vertex(cv.getX(), cv.getY(), cv.getZ()).color(red, green, blue, alpha).next();
+		}
+
+		tessellator.draw();
+	}
+}
---
+++ src/main/java/de/rangun/reanimator/config/ModMenu.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2022 by Heiko Schäfer <heiko@rangun.de>
+ *
+ * This file is part of ReAnimator.
+ *
+ * ReAnimator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * ReAnimator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ReAnimator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.rangun.reanimator.config;
+
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public final class ModMenu implements ModMenuApi {
+
+}
